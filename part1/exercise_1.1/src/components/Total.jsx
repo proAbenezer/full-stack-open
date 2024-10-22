76bce7551aec8f1d216ba1ef535eb055a51ba813
Abenezer Haile <devabenezer7@gmail.com>
2024-10-22 20:55:36 +0300

    exercise_1.1 step1 completed

--- part1/exercise_1.1/src/components/Total.jsx
+++
@@ -1,5 +0,0 @@
-const Total = (props) => {
-  return <p>Number of exercises {props.total}</p>;
-};
-
-export default Total;
---
+++ part1/src/App.jsx
@@ -0,0 +1,17 @@
+import Content from './components/Content';
+import Header from './components/Header';
+import Total from './components/Total';
+
+const App = () => {
+  const course = 'Half Stack application development';
+
+  return (
+    <div>
+      <Header course={course} />
+      <Content />
+      <Total />
+    </div>
+  );
+};
+
+export default App;
